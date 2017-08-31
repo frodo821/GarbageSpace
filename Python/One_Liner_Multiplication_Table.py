bc8455b87839759a44659e12b22c37aa47112c88
frodo821 <1234567890.sakai.jp@gmail.com>
2017-09-01 04:20:04 +0900

    Update One_Liner_Multiplication_Table.py

--- Python/One_Liner_Multiplication_Table.py
+++ Python/One_Liner_Multiplication_Table.py
@@ -1,1 +1,1 @@
-print('\n'.join(list(map(lambda x: '\t'.join(x), list(map((lambda x : map((lambda y: str(y)), x)), [[x * y for y in range(1,10)] for x in range(1,10)]))))))
+print('\n'.join(map(lambda x: '\t'.join(x), map((lambda x : map((lambda y: str(y)), x)), [[x * y for y in range(1,10)] for x in range(1,10)]))))
